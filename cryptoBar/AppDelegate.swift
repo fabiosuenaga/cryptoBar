9fbfc30d3d8daabd73f44f22fd124664383b34a7
Fabio Suenaga <fabiosuenaga@gmail.com>
2017-05-28 00:11:53 -0300

    Initial Commit

---
+++ cryptoBar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  cryptoBar
+//
+//  Created by Fabio Sato Suenaga on 28/05/17.
+//  Copyright © 2017 Fabio Sato Suenaga. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
